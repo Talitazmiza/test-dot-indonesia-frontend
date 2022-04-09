c40c6f4c7551783c3f4bdde6c668250c8fb22275
Talita <talitazmiza@gmail.com>
2022-04-09 14:05:23 +0700

    import Auth Provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+import {AuthProvider} from "./useAuth";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render (
+    <React.StrictMode>
+        <div className="yellow-border">
+            <div className="container">
+                <BrowserRouter>
+                    <AuthProvider>
+                        <App />
+                    </AuthProvider>
+                </BrowserRouter>
+            </div>
+        </div>
+    </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
